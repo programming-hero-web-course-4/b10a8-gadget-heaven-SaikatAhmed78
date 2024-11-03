d0fad7f80dd473d5c2a5d9baefdf2917d6e129ad
SaikatAhmed78 <shaikatahmed78@gmail.com>
2024-11-03 23:40:02 +0600

    Setup Tailwind, DeisyUI, and React Router

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom"
+
+
+
+const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: ''
+    },
+  ])
+
+  export default routes
